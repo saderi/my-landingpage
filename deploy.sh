9417bd177204e4f8204196fb91e79441c39ed260
payam <saderi@gmail.com>
2019-02-02 01:44:46 +0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+LOCALPATH='./dist'
+REMOTEPATH='/public_html'
+
+lftp -f "
+open ftp://$FTP_HOST
+user $FTP_USER $FTP_PASSWORD
+mirror --continue --reverse $LOCALPATH $REMOTEPATH
+bye
+" 
