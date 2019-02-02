592f4ccca23657fb7eae4cd63f5e00e3a90531be
payam <saderi@gmail.com>
2019-02-02 03:32:52 +0300

    Add some config for disable SSL verification :D

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
-echo set ssl:verify-certificate false > ~/.lftprc
+echo 'set ssl:verify-certificate off;' > ~/.lftprc
+echo 'set ftp:ssl-allow off;' >> ~/.lftprc
 
 LOCALPATH='./dist'
 REMOTEPATH='/public_html'
 lftp -f "
 open ftp://$FTP_HOST
 user $FTP_USER $FTP_PASSWORD
-mirror --continue --reverse --delete $LOCALPATH $REMOTEPATH
+mirror --continue --reverse $LOCALPATH $REMOTEPATH
 bye
 " 
 
