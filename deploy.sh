f583af3a9a154744d531f60a0edff9a23cd1838b
payam <saderi@gmail.com>
2019-02-02 04:23:32 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo 'set ssl:verify-certificate off;' > ~/.lftprc
 echo 'set ftp:ssl-allow off;' >> ~/.lftprc
 
 LOCALPATH='./dist'
-REMOTEPATH='/public_html'
+REMOTEPATH='/public_html/test'
 
 lftp -u ${FTP_USER},${FTP_PASSWORD} ${FTP_HOST} << EOF
-mirror --continue  ${LOCALPATH} ${REMOTEPATH}
+mirror --continue --reverse ${LOCALPATH} ${REMOTEPATH}
 bye
 EOF
