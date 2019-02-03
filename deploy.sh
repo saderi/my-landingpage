0bb30ca8914d7c08801ba8845d8ee153ae27f645
payam <saderi@gmail.com>
2019-02-03 13:45:28 +0300

    Test Travis with WebDisk

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 set -e
-echo 'set ssl:verify-certificate off;' > ~/.lftprc
-echo 'set ftp:ssl-allow off;' >> ~/.lftprc
+
+# define SERVER_WEBDISK in travis Environment Variables 
+# define HOST_USERNAME in travis Environment Variables 
+# define HOST_PASSWORD in travis Environment Variables 
 
 LOCALPATH='./dist'
-REMOTEPATH='/public_html/test'
+REMOTEPATH='/public_html'
 
-lftp -u ${FTP_USER},${FTP_PASSWORD} ${FTP_HOST} << EOF
-mirror --continue --reverse --ascii ${LOCALPATH} ${REMOTEPATH}
-bye
-EOF
+mapfile -t DIR_LIST < <(tree -ifp --noreport $LOCALPATH)
+for (( i=1; i<${#DIR_LIST[@]}; i++ ));
+do 
+    if [ ${DIR_LIST[i]:1:1} = 'd' ]; then
+        # Create Directorys
+        DIR_PATH=$(echo ${DIR_LIST[i]} | awk {'print $2'})
+        DIR_NAME=${DIR_PATH#"$LOCALPATH"}
+        curl -u $HOST_USERNAME:$HOST_PASSWORD -X MKCOL "$SERVER_WEBDISK$REMOTEPATH$DIR_NAME" >/dev/null 2>&1
+    else
+        # Upload file
+        LOCAL_FILE_PATH=$(echo ${DIR_LIST[i]} | awk {'print $2'})
+        FILE_PATH=${LOCAL_FILE_PATH#"$LOCALPATH"}
+        mapfile -t FILE_PATH_LIST < <(echo $FILE_PATH | tr "/" "\n")
+        FILE_NAME=$(echo ${FILE_PATH_LIST[${#FILE_PATH_LIST[@]}-1]})
+        REMOTE_DIR_NAME=$(echo ${FILE_PATH%"$FILE_NAME"})
+        curl  -u $HOST_USERNAME:$HOST_PASSWORD -T $LOCAL_FILE_PATH "$SERVER_WEBDISK$REMOTEPATH$REMOTE_DIR_NAME" >/dev/null 2>&1
+    fi
+done
+
+
